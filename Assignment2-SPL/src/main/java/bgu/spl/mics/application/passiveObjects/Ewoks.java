fb4113ee80e130e216f0c663b2117bd21be4df7d
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2020-12-04 12:42:31 +0200

    Update Ewoks.java

--- Assignment2-SPL/src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
+++ Assignment2-SPL/src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
@@ -1,6 +1,8 @@
 package bgu.spl.mics.application.passiveObjects;
 
 
+import java.util.ArrayList;
+
 /**
  * Passive object representing the resource manager.
  * <p>
@@ -10,5 +12,17 @@ package bgu.spl.mics.application.passiveObjects;
  * You can add ONLY private methods and fields to this class.
  */
 public class Ewoks {
+    private static Ewoks ewoksInstance = null;
+    private ArrayList<Ewok> ewoksList;
 
+    private Ewoks(){
+        //Ewoks going to be a list of every single ewok that the program including
+        ewoksList=new ArrayList<>();
+    }
+
+    public static Ewoks getInstance(){
+        if(ewoksInstance == null)
+            ewoksInstance=new Ewoks();
+        return ewoksInstance;
+    }
 }
