2203d5f232e87afb9cbc5e3437431d9871ad5d5e
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2020-12-06 15:54:50 +0200

    update "AttackEvents" to "AttackEvent"

--- Assignment2-SPL/src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
+++ Assignment2-SPL/src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
@@ -7,9 +7,9 @@ import bgu.spl.mics.application.messages.AttackEvent;
 import bgu.spl.mics.application.messages.DeactivationEvent;
 
 /**
- * HanSoloMicroservices is in charge of the handling {@link AttackEvents}.
+ * HanSoloMicroservices is in charge of the handling {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
